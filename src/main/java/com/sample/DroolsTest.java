91f607b7403b850aa88b120ca0ecaccf9ea1d3d4
zxia <zxia@liacs.nl>
2017-09-14 22:20:15 +0200

    try out map access

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -2,7 +2,9 @@ package com.sample;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
@@ -29,7 +31,25 @@ public class DroolsTest {
         	customer.set(ce1, "id", 123L);
         	customer.set(ce1, "age", 19);
         	customer.set(ce1, "firstName", "Pendragon");
-        	customer.set(ce1, "lastName", "Arthur");
+        	customer.set(ce1, "lastName", "Artoria");
+        	
+        	Map<String, Object> stackholders = new HashMap<String, Object> ();
+        	
+        	Object wife = customer.newInstance();
+        	customer.set(wife, "id", 124L);
+        	customer.set(wife, "age", 18);
+        	customer.set(wife, "firstName", "Guenevere");
+        	customer.set(wife, "lastName", "Arthur");
+        	stackholders.put("wife", wife);
+        	
+        	Object son = customer.newInstance();
+        	customer.set(son, "id", 125L);
+        	customer.set(son, "age", 12);
+        	customer.set(son, "firstName", "Mordred");
+        	customer.set(son, "lastName", "Arthur");
+        	stackholders.put("son", son);
+        	
+        	customer.set(ce1, "stakeholders", stackholders);
         	
         	List<Object> subscriptions = new ArrayList<> ();
         	
@@ -57,17 +77,17 @@ public class DroolsTest {
         	FactType article = kbase.getFactType("com.sample", "Article");
         	
             Object article1 = article.newInstance();
-            article.set(article1, "name", "article1");
+            article.set(article1, "name", "\"The fight between father and son\"");
             article.set(article1, "tag", 0);
             articles.add(article1);
             
             Object article2 = article.newInstance();
-            article.set(article2, "name", "article2");
+            article.set(article2, "name", "\"How to wield Excalibur\"");
             article.set(article2, "tag", 1);
             articles.add(article2);
             
             Object article3 = article.newInstance();
-            article.set(article3, "name", "article3");
+            article.set(article3, "name", "\"How to build the table\"");
             article.set(article3, "tag", 2);
             articles.add(article3);
             
